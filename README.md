83df80db239c55f9b9fef4e4c2477fcbefc0389d
ejn2 <eduardop9011@gmail.com>
2021-04-29 16:50:22 -0300

    pyserver

---
+++ README.md
@@ -0,0 +1,6 @@
+<h2>Servidor Json</h2>
+<hr>
+<p>Quando você está construindo uma Aplicação web e ainda não tem uma api implementada, você pode usar esse pequeno servidor em python para disponibilizar um arquivo json como uma api.</p>
+
+<br>
+<p style="background-color: red; padding: 1rem; border-radius: 0.700rem; font-family: arial; font-size: 1.1rem;">! Esse é um simples código para testes.</p>
---
+++ server.py
@@ -0,0 +1,28 @@
+from socket import *
+from os import path
+
+#####################################################
+#        Servidor python para arquivos json         #
+#####################################################
+
+IP = '127.0.0.1'
+PORT = 3003
+
+data = open(path.join('./data.json')).read() #Faz a leitura do arquivo selecionado.
+
+server = socket(AF_INET, SOCK_STREAM)
+server.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+server.bind((IP, PORT))
+server.listen(7)
+
+print(f"|Running...\n|=> http://{IP}:{PORT}\n") #Informa quando o servidor está "rodando"
+
+while True:
+    cli, addr = server.accept()
+    
+    req = cli.recv(1024) #Contêm dados das requisições
+
+    cli.send(b"HTTP/1.1 200 ok\r\n")
+    cli.send(b"Content-Type: application/json\r\n\r\n") #Envia header informando o tipo de conteudo.
+    cli.send(data.encode())
+    cli.close()
